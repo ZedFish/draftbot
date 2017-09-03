3cf88df0e3cb42ae4c488de930ee14cab69d5627
Miller Richey <miller@richy.be>
2017-09-03 05:55:15 +0000

    run.sh saving

--- run.sh
+++ run.sh
@@ -1,17 +1,11 @@
   echo "updating from git.."
   git pull
-  
+
   echo "updating gems.."
   bundle update
-  
+
   echo "installing gems.."
   bundle install
-
-  echo "running rubocop.."
-  rubocop lib
-
-  echo "updating documentation.."
-  yardoc lib
 
   echo "starting bot.."
   bundle exec ruby main.rb
